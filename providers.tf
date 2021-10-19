92417a2cb9145e1033510b9d20a9254aa9dafb14
Emre <emrecosarr@gmail.com>
2021-10-19 16:23:30 +0200

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "~> 1.0.9"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "polarissquad.fun-terraform"
+    key    = "prod/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "us-east-1"
+}
